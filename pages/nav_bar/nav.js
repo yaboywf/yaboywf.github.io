c2d75914b3901c7c6a3f25fdef0a7299daacd0ae
Dylan <dylanyeowf@gmail.com>
2025-04-01 12:41:39 +0800

    Fixed bug in nav

--- pages/nav_bar/nav.js
+++ pages/nav_bar/nav.js
@@ -1,2 +1,6 @@
 const path = window.parent.location.pathname.replace("/", "").replace(".html", "");
+if (path === "") {
+    document.getElementById("index").setAttribute("active", "true");
+    return;
+}
 document.getElementById(path).setAttribute("active", "true");
