01ade75ab253c5ba1b2a74bb9d7441ad2ae3d42f
Dylan <dylanyeowf@gmail.com>
2025-05-08 13:44:42 +0800

    updated routing configurations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,12 @@ import "../styles/nav.scss";
 function Header() {
     const [activeLink, setActiveLink] = useState("/");
 
-    useEffect(() => {
-        const path = window.location.pathname;
-        console.log(path)
-        setActiveLink(path);
-    }, []);
+    useEffect(() => setActiveLink(window.location.hash), []);
 
     return (
         <nav>
-            <a href="/" target="_top" id="index" className={activeLink === "/" ? "active" : ""}>Home</a>
-            <a href="/experience" target="_top" id="experience" className={activeLink === "/experience" ? "active" : ""}>Experience</a>
+            <a href="/" target="_top" id="index" className={activeLink === "" ? "active" : ""}>Home</a>
+            <a href="/#/experience" target="_top" id="experience" className={activeLink === "#/experience" ? "active" : ""}>Experience</a>
         </nav>
     );
 }
