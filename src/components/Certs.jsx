e756f04f02aa7a916a707e280bbbcfd887ffc438
Dylan <dylanyeowf@gmail.com>
2025-08-18 18:43:53 +0800

    optimised certs section

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -18,12 +18,12 @@ const Certs = () => {
             <h2>Certificates</h2>
             <div>
                 <div className='controls'>
-                    <i className='fas fa-chevron-left' aria-label="Previous certificate" onClick={() => setCertIndex(certIndex === 1 ? 13 : certIndex - 1)}></i>
+                    <i className='fas fa-chevron-left' onClick={() => setCertIndex(certIndex === 1 ? 13 : certIndex - 1)}></i>
                     <div className='cert_photo' style={{ background: `url('/images/cert${certIndex}.webp') center/contain no-repeat` }}></div>
-                    <i className='fas fa-chevron-right' aria-label="Next certificate" onClick={() => setCertIndex(certIndex === 13 ? 1 : certIndex + 1)}></i>
+                    <i className='fas fa-chevron-right' onClick={() => setCertIndex(certIndex === 13 ? 1 : certIndex + 1)}></i>
                 </div>
                 <div className='certs_list'>
-                    {content.map(contentItem => <input key={contentItem} type="radio" name="certs" id={contentItem} onChange={() => setCertIndex(parseInt(contentItem))} role="button" aria-label={`Select Certificate ${contentItem}`} checked={certIndex === parseInt(contentItem)} />)}
+                    {content.map(contentItem => <input key={contentItem} type="radio" name="certs" id={contentItem} onChange={() => setCertIndex(parseInt(contentItem))} role="button" checked={certIndex === parseInt(contentItem)} />)}
                 </div>
             </div>
         </section>
